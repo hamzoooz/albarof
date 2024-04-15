068a6259eff97625ba6fcbda1a57ebc360245e4f
Hamza  Banga <hamzoooz123@gmail.com>
2024-04-15 12:57:16 +0300

    init

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Service(models.Model):
+    title =   models.CharField(("title"), max_length=50)
+    description = models.CharField(("Description"), max_length=150)
+    icon = models.CharField(("icon name"), max_length=50)
+    
