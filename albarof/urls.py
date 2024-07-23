6cd53cd8d5ab0586a9fd9150976080e01f7743e9
hamzoooz <hamzoooz123@gmail.com>
2024-07-23 12:31:10 +0000

    init

--- albarof/urls.py
+++ albarof/urls.py
@@ -8,9 +8,14 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('core.urls')),
-    
+
 ]
 
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
